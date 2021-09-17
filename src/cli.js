582c21cb33d0511789f759d0445bc9ef0c0cddfa
Ksenia <k.vetkina@mail.ru>
2021-09-17 16:53:07 +0300

    eslint - task-4

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export function hello() {
-var userName = readlineSync.question('May I have your name? ');
-console.log('Hi ' + userName + '!');
+function hello() {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hi ${userName}!`);
 }
 
+export { hello as default };
